416674ed17acd8293b388b710302fc00579e0bec
nbnaradhya <nikhilbnaradhya1.gem@gmail.com>
2018-12-16 16:49:07 +0530

    s

--- postFeed.js
+++ postFeed.js
@@ -3,7 +3,7 @@ FB.api(
   'GET',
   {"fields":"id,name,feed{created_time,attachments,message,story}"},
   function(response) {
-      var posts=JSON.parse(response);
+      var posts=JSON.parse("response");
       document.getElementById(postfeed).innerHTML=posts.feed.data[0].message;
   }
 );
