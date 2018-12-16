17efd2bc2578c2bc38e26934eec8c317871f4015
nbnaradhya <nikhilbnaradhya1.gem@gmail.com>
2018-12-16 22:00:43 +0530

    s

--- postFeed.js
+++ postFeed.js
@@ -5,14 +5,6 @@
       xfbml            : true,
       version          : 'v3.2'
     });
-    FB.api(
-      '/me',
-      'GET',
-      {"fields":"id,name,feed{created_time,attachments,message,story}"},
-      function(response) {
-        console.log(response);
-      }
-    );
   };
 
   (function(d, s, id){
@@ -22,3 +14,12 @@
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
+   FB.api(
+     '/me',
+     'GET',
+     {"fields":"id,name,feed{created_time,attachments,message,story}"},
+     function(response) {
+       console.log(response);
+     }
+   );
