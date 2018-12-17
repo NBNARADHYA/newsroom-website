7baa3cee16bdf3e0ac15207fb5c5a3a190ce0caa
nbnaradhya <nikhilbnaradhya1.gem@gmail.com>
2018-12-17 22:58:15 +0530

    s

--- facebook.js
+++ facebook.js
@@ -49,7 +49,7 @@ function printPosts(response){
     posts += ("<b>" + nam + "</b><br>" + response.feed.data[i].created_time.substr(8,2) + " " +
   getMonth(parseInt((response.feed.data[i].created_time.substr(5,1)=='0') ?
   response.feed.data[i].created_time.substr(6,1) : response.feed.data[i].created_time.substr(5,2)))
-  + response.feed.data[i].created_time.substr(0,4) + "<br>" + response.feed.data[i].message + "<br>");
+  + " " + response.feed.data[i].created_time.substr(0,4) + "<br>" + response.feed.data[i].message + "<br>");
   }
   document.getElementById("postfeed").innerHTML = posts;
 }
