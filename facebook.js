bb7d2063d6af94a3160bad3d4e03043bde3d8150
nbnaradhya <nikhilbnaradhya1.gem@gmail.com>
2018-12-17 22:53:23 +0530

    s

--- facebook.js
+++ facebook.js
@@ -33,6 +33,7 @@ window.fbAsyncInit = function() {
        function(response){
          if(response && !response.error){
            console.log(response);
+           printPosts(response);
          }
        }
      );
@@ -41,6 +42,33 @@ window.fbAsyncInit = function() {
      console.log("Not authenticated");
    }
  }
+
+function printPosts(response){
+  var len = response.feed.data.length, nam = response.name, posts = "";
+  for(i=0 ; i<len; i++){
+    posts += ("<b>" + nam + "</b><br>" + response.feed.data[i].created_time.subsrt(8,2) + " " +
+  getMonth(parseInt((response.feed.data[i].created_time.substr(5,1)=='0') ?
+  response.feed.data[i].created_time.substr(6,1) : response.feed.data[i].created_time.substr(5,2)))
+  + response.feed.data[i].created_time.substr(0,4) + "<br>" + response.feed.data[i].message + "<br>");
+  }
+}
+
+function getMonth(month){
+  switch (month) {
+    case 1: return "January"; break;
+    case 2: return "February"; break;
+    case 3: return "March"; break;
+    case 4: return "April"; break;
+    case 5: return "May"; break;
+    case 6: return "June"; break;
+    case 7: return "July"; break;
+    case 8: return "August"; break;
+    case 9: return "September"; break;
+    case 10: return "October"; break;
+    case 11: return "November"; break;
+    case 12: return "December"; break;
+  }
+}
 
  function checkLoginState() {
    FB.getLoginStatus(function(response) {
