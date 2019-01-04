068d660924f0acc173af156be8e2db641ee37e48
nbnaradhya <nikhilbnaradhya1.gem@gmail.com>
2019-01-04 23:58:09 +0530

    extension problem solved

--- extension.js
+++
@@ -1,6 +0,0 @@
-var isFirefox = typeof InstallTrigger !== 'undefined';
-
-if (isFirefox && !document.getElementsByClassName("expected-extension-id").length) {
-  alert("Please install a Firefox add-on Ignore X-Frame-Options Header");
-  window.location.href = "https://addons.mozilla.org/en-US/firefox/addon/ignore-x-frame-options-header/";
-}
