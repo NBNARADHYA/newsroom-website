a805ab7231a886ad67e397d163c727d6071c6ffd
nbnaradhya <nikhilbnaradhya1.gem@gmail.com>
2019-01-08 12:08:08 +0530

    made some changes to fb.js

--- facebook.js
+++ facebook.js
@@ -50,7 +50,7 @@ window.fbAsyncInit = function() {
        }
      );
      FB.api(
-       '/379659976120888',
+       '/359429951528331',
        'GET',
        {"fields": "id,name,picture,feed{created_time,message,attachments}"},
        function(response){
@@ -69,7 +69,7 @@ function statusChangeCallback1(response) {
   if(response.status === "connected"){
     console.log("Authenticated");
     FB.api(
-      '/379659976120888',
+      '/359429951528331',
       {"fields": "id,name,picture,feed{created_time,message,attachments}"},
       function(response){
         if(response && !response.error){
