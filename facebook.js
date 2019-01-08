b022b6817839df5abd12f2213e358b3c28c98db2
nbnaradhya <nikhilbnaradhya1.gem@gmail.com>
2019-01-08 22:07:54 +0530

    cookies are back

--- facebook.js
+++ facebook.js
@@ -45,7 +45,7 @@ window.fbAsyncInit = function() {
        {"fields": "name"},
        function(response){
          if(response && !response.error){
-          // checkCookie(response.name);
+           var pageIdArray = checkCookie(response.name);
            alert("Welcome again " + response.name + "!");
          }
        }
@@ -364,6 +364,121 @@ function recentPosts() {
   document.getElementById("postfeed").style.display = "block";
   document.getElementById("badge").style.display = "none";
 }
+
+var cookie = "";
+
+function setCookie(exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  cookie += expires + ";path=/";
+  document.cookie = cookie;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie(name) {
+  var user = getCookie("username");
+  if (user === name && user !== "") {
+    const len = getCookie("length");
+    var pageIdArray = "";
+    for (var i = 0; i < len; i++) {
+      if(i !== len - 1){
+        pageIdArray += getCookie("pageId" + i) + ",";
+      } else {
+        pageIdArray += getCookie("pageId" + i);
+      }
+    }
+    return pageIdArray;
+  } else {
+    document.getElementById("pageSelection").style.display = "block";
+    document.getElementById("postfeed").style.display = "none";
+    cookie += "username=" + name + ";";
+    document.getElementById("submitButton").addEventListener("click" , function(){
+      var pages = document.getElementsByClassName("checkboxClass");
+      var len = 0;
+      for (var i = 0; i < pages.length; i++) {
+        if (pages[i].checked) {
+          len ++;
+        }
+      }
+      cookie += "length=" + len + ";";
+      var j = 1;
+      for (var i = 0; i < pages.length; i++) {
+        if (pages[i].checked) {
+          cookie += "pageId" + j + "=" + pages[i].value + ";";
+          j ++;
+        }
+      }
+      setCookie(365);
+      document.getElementById("pageSelection").style.display = "none";
+      document.getElementById("postfeed").style.display = "block";
+    });
+    const len = getCookie("length");
+    var pageIdArray = "";
+    for (var i = 0; i < len; i++) {
+      if(i !== len - 1){
+        pageIdArray += getCookie("pageId" + i) + ",";
+      } else {
+        pageIdArray += getCookie("pageId" + i);
+      }
+    }
+    return pageIdArray;
+  }
+}
+
+function getPages() {
+  var pages = document.getElementsByClassName("checkboxClass");
+  var len = 0;
+  for (var i = 0; i < pages.length; i++) {
+    if (pages[i].checked) {
+      len ++;
+    }
+  }
+  cookie += "length=" + len + ";";
+  var j = 1;
+  for (var i = 0; i < pages.length; i++) {
+    if (pages[i].checked) {
+      cookie += "pageId" + j + "=" + pages[i].value + ";";
+      j ++;
+    }
+  }
+  setCookie(365);
+  document.getElementById("pageSelection").style.display = "none";
+  document.getElementById("postfeed").style.display = "block";
+}
+
+function checkAll(ele) {
+   var checkboxes = document.getElementsByTagName('input');
+   if (ele.checked) {
+       for (var i = 0; i < checkboxes.length; i++) {
+           if (checkboxes[i].type == 'checkbox') {
+               checkboxes[i].checked = true;
+           }
+       }
+   } else {
+       for (var i = 0; i < checkboxes.length; i++) {
+           if (checkboxes[i].type == 'checkbox') {
+               checkboxes[i].checked = false;
+           }
+       }
+   }
+}
+
 
  function log_out() {
    FB.logout(function(response){
